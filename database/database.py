8f6fbb96d9e0f58e1fde1cf58ce4d5488ef645af
Strahinja Trninic <87874998+StrahinjaT97@users.noreply.github.com>
2024-11-14 19:37:57 +0100

    Add files via upload

--- database/database.py
+++ database/database.py
@@ -2,6 +2,13 @@ import pandas as pd
 import csv
 import sys
 import os
+import mysql.connector as mysql
+
+#sys.argv[1] = ime robijevog excel fajlsa
+#sys.argv[2] = host
+#sys.argv[3] = user
+#sys.argv[4] = pass
+#sys.argv[5] = database
 
 class DatabaseEntry:
     def __init__(self):
@@ -19,3 +26,11 @@ with open("database.csv", newline="") as csv_file:
 os.remove("database.csv")
 
 
+conn = mysql.connect(host = sys.argv[2], user = sys.argv[3], password = sys.argv[4], database = sys.argv[5])
+
+cursor = conn.cursor()
+
+# sql = "neki sql statement sa %s itd parametrima"
+# val = [(x11, x12, ..., x1n), ..., (xm1, xm2, ..., xmn)]
+# cursor.executemany(sql, val)
+# conn.commit()
