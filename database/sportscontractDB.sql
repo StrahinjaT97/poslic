13f1f520e65a2bce6d8f4929852996f790ff88d0
Strahinja Trninic <87874998+StrahinjaT97@users.noreply.github.com>
2024-11-17 19:46:27 +0100

    Update sportscontractDB.sql

--- database/sportscontractDB.sql
+++ database/sportscontractDB.sql
@@ -5,28 +5,35 @@ CREATE TABLE LIGA (
   Name varchar(255) NOT NULL,
   BucketName varchar(255) NOT NULL,
   FormatNazivFajla varchar(255) NOT NULL,
-  SajtId int FOREIGN KEY REFERENCES SAJT(SajtId),
+  SajtId int NOT NULL,
+  CONSTRAINT SajtId FOREIGN KEY (SajtId) REFERENCES SAJT(SajtId),
   UrlGuid varchar(255) NOT NULL
 );
 
 CREATE TABLE DIVIZIJA (
   DivizijaId int PRIMARY KEY,
-  LigaId int FOREIGN KEY REFERENCES LIGA(LigaId),
-  SajtId int FOREIGN KEY REFERENCES SAJT(SajtId),
-  UrlGuid varchar(255) NOT NULL,
+  LigaId int NOT NULL,
+  SajtId int NOT NULL,
+  CONSTRAINT LigaId FOREIGN KEY (LigaId) REFERENCES liga(LigaId),
+  CONSTRAINT SatjId FOREIGN KEY (SajtId) REFERENCES sajt(SajtId),
+  UrlGuid varchar(255) NOT NULL
 );
 
 CREATE TABLE TIM (
   TimId int PRIMARY KEY,
   SiteName varchar(255),
-  SajtId int FOREIGN KEY REFERENCES SAJT(SajtId),
-  FileName varchar(255) NOT NULL
+  SajtId int NOT NULL,
+  FileName varchar(255) NOT NULL,
+  CONSTRAINT SatjId FOREIGN KEY (SajtId) REFERENCES sajt(SajtId)
 );
 
 CREATE TABLE SAJT(
-  SajtId int PRIMARY KEY,
+  SajtId int,
   Url varchar(255) NOT NULL,
+  PRIMARY KEY (SajtId)
 );
+
+
 
 CREATE TABLE DULTask(
   DULTaskId int PRIMARY KEY,
@@ -34,10 +41,22 @@ CREATE TABLE DULTask(
   ErrorMessage varchar(65535),
   TaskStartDateAndTime DATETIME DEFAULT CURRENT_TIMESTAMP(),
   TaskEndDateAndTime DATETIME DEFAULT CURRENT_TIMESTAMP(),
-  LigaId int FOREIGN KEY REFERENCES LIGA(LigaId),
-  DivizijaId int FOREIGN KEY REFERENCES DIVIZIJA(DivizijaId),
-  HomeTeamId varchar FOREIGN KEY REFERENCES TIM(TimId),
-  AwayTeamId varchar FOREIGN KEY REFERENCES TIM(TimId),
+  LigaId int,
+  DivizijaId int,
+  HomeTeamId varchar,
+  AwayTeamId varchar,
   GameDateAndTime DATETIME DEFAULT CURRENT_TIMESTAMP(),
+  CONSTRAINT LigaId FOREIGN KEY REFERENCES LIGA(LigaId),
+  CONSTRAINT DivizijaId FOREIGN KEY REFERENCES DIVIZIJA(DivizijaId),
+  CONSTRAINT HomeTeamId FOREIGN KEY REFERENCES TIM(TimId),
+  CONSTRAINT AwayTeamId FOREIGN KEY REFERENCES TIM(TimId)
 );
+
+
+
+
+
+
+
+
 
