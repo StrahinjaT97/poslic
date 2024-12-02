811eb6cfa9c2110e709b5564dfb08e45e914f36b
Strahinja Trninic <87874998+StrahinjaT97@users.noreply.github.com>
2024-12-02 16:43:00 +0100

    Delete scheduler.py

--- scheduler.py
+++
@@ -1,4 +0,0 @@
-import os
-
-def schedule_task(task_name, script_path, start_date, start_time):
-    os.system("..\\res\\scheduler.bat " + task_name + " " + script_path + " " + start_date + " " + start_time)
